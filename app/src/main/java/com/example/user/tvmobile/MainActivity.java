2f33e4395ad8ff8b2798e7dcc07cbeef34f61d67
syntaxSizer <kakuma4never@gmail.com>
2016-06-15 17:17:12 +0300

    picasso library app

--- app/src/main/java/com/example/user/tvmobile/MainActivity.java
+++ app/src/main/java/com/example/user/tvmobile/MainActivity.java
@@ -1,22 +1,45 @@
 package com.example.user.tvmobile;
 
-import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
 
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by geckozila on 15/06/16.
+ */
 public class MainActivity extends AppCompatActivity {
 
+    ImageView image;
+    private static final String Tag = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        image =(ImageView) findViewById(R.id.iv_fan_art);
+        String imageUri = "https://unsplash.com/photos/1CsfTx0DuLs";
+        Picasso picasso = new Picasso.Builder(this).listener(new Picasso.Listener(){
+            @Override
+            public void onImageLoadFailed(Picasso picasso, Uri uri, Exception exception) {
+                exception.printStackTrace();
+        }
 
-    private void initializeDraggablePanel() throws Resources.NotFoundException {
-        draggablePanel.setFragmentManager(getSupportFragmentManager());
-        draggablePanel.setTopFragment(placeFragment);
-        draggablePanel.setBottomFragment(mapFragment);
-        draggablePanel.initializeView();
-    }
+        }).build();
+        picasso.load(imageUri).into(image, new Callback() {
+            @Override
+            public void onSuccess() {
+                Log.d(Tag, "Success");
+            }
 
+            @Override
+            public void onError() {
+                Log.d(Tag , "Error");
+
+            }
+        });
+    }
 }
